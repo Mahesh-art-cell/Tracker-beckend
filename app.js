0e65ab03b099bf99279ead3fe909ea24c572ac20
Mahesh Talluri <mtalluri563@gmail.com>
2025-05-27 10:45:02 +0530

    update

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://tracker-fontend.vercel.app/"
+}));
 
 // ADD THIS: Root route to fix "Cannot GET /" error
 app.get('/', (req, res) => {
